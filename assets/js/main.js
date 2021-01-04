780e99343ca5648a84edfa024eeccb4624585f40
No1RajAryan <55110091+No1RajAryan@users.noreply.github.com>
2021-01-04 20:14:35 +0530

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -75,6 +75,21 @@ sr.reveal('.contact__text', {interval: 200})
 sr.reveal('.contact__input', {delay: 400})
 sr.reveal('.contact__button', {delay: 600})
 
+// inspect mode disable code 
 
+document.onkeydown=function(e)
+{
+    if(event.keyCode == 123)
+    {
+        return false;
+    }
+    if (e.ctrlKey && e.shiftKey && e.keyCode == 'I'.charCodeAt(0)){
+        return false ;
+    }
+    if(e.ctrlKey && e.keyCode == 'U'.charCodeAt(0)){
+        return false;
+
+    }
+}
 
 
